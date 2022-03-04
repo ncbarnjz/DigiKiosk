fd6053c56379995397d4e7816d3f7e645a712d1d
NIEL-DEKSTOP\nielcleo <nielcleo@gmail.com>
2022-03-04 22:11:22 +0800

    Add num pad

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 
 @Component({
@@ -10,12 +12,21 @@ export class HomeComponent implements OnInit {
 
   mobile: string = "";
   data: any;
+  
 
-  constructor(private localService: LocalStorageService) { }
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private localService: LocalStorageService) {
+
+  }
 
   ngOnInit() {
     this.mobile = this.localService.get('mobile');
     this.data = this.localService.get('details');
+  }
+
+  submit(mobile: any) {
+    alert(mobile);
   }
 
 }
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   declarations: [HomeComponent]
 })
