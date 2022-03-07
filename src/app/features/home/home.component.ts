46bb55a72a318986054be1e2a3985fb16e75ba15
NIEL-DEKSTOP\nielcleo <nielcleo@gmail.com>
2022-03-07 22:58:06 +0800

    Add countdown

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription, timer } from 'rxjs';
 import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 
 @Component({
@@ -13,11 +14,20 @@ export class HomeComponent implements OnInit {
   mobile: string = "";
   data: any;
   
+  countDown:Subscription | undefined;
+  counter = 30;
+  tick = 1000;
 
   constructor(private fb: FormBuilder,
     private router: Router,
     private localService: LocalStorageService) {
-
+      this.countDown = timer(0, this.tick)
+      .subscribe(() => {
+        if (this.counter > 0) {
+          --this.counter;
+        }      
+      });
+      
   }
 
   ngOnInit() {
